e72a1dcb716b26a63065688088865d25b0aa9008
Saptarshi Mandal <121552564+Saptarshi-108@users.noreply.github.com>
2026-01-09 12:33:45 +0530

    Create Switch_Case.java
    9th jan 2026

---
+++ Switch_Case.java
@@ -0,0 +1,27 @@
+package Java_training;
+
+public class Switch_Case {
+	public static void main(String[] args) {
+		switch (2) {
+		case 1:{
+			System.out.println("c1");
+			break;
+		}
+		case 2:{
+			System.out.println("c2");
+			break;
+		}
+		case 3:{
+			System.out.println("c3");
+			break;
+		}
+		case 4:{
+			System.out.println("c4");
+			break;
+		}
+		default:{
+			System.out.println("no match");
+		}
+	}
+}
+}
