335c0d1fac7a29ca1f14df8b331918ed7c238006
Saptarshi Mandal <121552564+Saptarshi-108@users.noreply.github.com>
2026-01-09 12:45:34 +0530

    Create Calculator_switchcase.java

---
+++ Calculator_switchcase.java
@@ -0,0 +1,30 @@
+package Java_training;
+
+import java.util.Scanner;
+
+// Write a program to create a calculator by using swtich case.
+public class Calculator_switchcase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        char c = sc.next().charAt(0);
+        switch (c) {
+            case '+':
+                System.out.println(a + b);
+                break;
+
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                System.out.println(a / b);
+                break;
+            default:
+                System.out.println("no match");
+        }
+    }
+}
