83b9154bc91f8e9c2d16a13d8f6e522900a2f4dd
Saptarshi Mandal <121552564+Saptarshi-108@users.noreply.github.com>
2026-01-09 09:21:38 +0530

    Typecasting assignment

---
+++ Demo.java
@@ -0,0 +1,22 @@
+package Java_training;
+
+public class Demo {
+	public static void main(String[] args) {
+		
+		byte a = 0;
+		float b = a;
+		System.out.println(b);
+		
+		float c = 99.9f;
+		int d=(int)c;
+		System.out.println(d);
+		
+		char e = 'a';
+		int j = e;
+		System.out.println(j);
+		
+		int n = 67;
+		char x = (char) n;
+		System.out.println(x);
+	}
+}
