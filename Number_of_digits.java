b92893b6d91e9c57399797cbb18bcc8801024ed9
Saptarshi Mandal <121552564+Saptarshi-108@users.noreply.github.com>
2026-01-09 22:51:30 +0530

    Create Number_of_digits.java
    9th jan 2026

---
+++ Number_of_digits.java
@@ -0,0 +1,20 @@
+package Java_training;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int count = 0;
+
+        while (number != 0) {
+            count++;
+            number = number / 10;
+        }
+
+        System.out.println(count);
+        sc.close();
+    }
+}
