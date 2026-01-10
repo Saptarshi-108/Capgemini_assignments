b12f63f2cf76b13d4e5c8a27bd8ee6e27c63e18c
Saptarshi Mandal <121552564+Saptarshi-108@users.noreply.github.com>
2026-01-10 10:41:48 +0530

    Implement calculator using switch case
    new method for doing calculator. Pew pew

--- Calculator_switchcase.java
+++ Calculator_switchcase.java
@@ -28,3 +28,42 @@ public class Calculator_switchcase {
         }
     }
 }
+
+//Write a calculator using method and case switch.
+
+package Java_training;
+
+import java.util.Scanner;
+
+public class Solution {
+	public static void Calculator(int a, int b, char c) {
+		switch (c) {
+		case '+':
+			System.out.println(a + b);
+			break;
+
+		case '-':
+			System.out.println(a - b);
+			break;
+		case '*':
+			System.out.println(a * b);
+			break;
+		case '/':
+			System.out.println(a / b);
+			break;
+		default:
+			System.out.println("no match");
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		char c = sc.next().charAt(0);
+
+		Calculator(a, b, c);
+	}
+}
+
