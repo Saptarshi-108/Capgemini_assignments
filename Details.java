ff8a965794e66d43d279f9fecea8ad2e00b0c5cc
Saptarshi Mandal <121552564+Saptarshi-108@users.noreply.github.com>
2026-01-10 09:50:54 +0530

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,20 @@
+package Java_training;
+
+public class Details {
+	 public static void main(String[] args) {
+
+	   String name = "Saptarshi Mandal";
+       long phoneNumber = 9876543210L;
+       char gender = 'M';
+       boolean married = false;
+       int age = 22;
+       float weight = 65.5f;
+
+       System.out.println("Name: " + name);
+       System.out.println("Phone Number: " + phoneNumber);
+       System.out.println("Gender: " + gender);
+       System.out.println("Married: " + married);
+       System.out.println("Age: " + age);
+       System.out.println("Weight: " + weight);
+	 }
+}
