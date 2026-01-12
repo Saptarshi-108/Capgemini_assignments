c6155a85b1ef48ff141cc3875499f473f2460e4e
Saptarshi Mandal <121552564+Saptarshi-108@users.noreply.github.com>
2026-01-13 00:22:27 +0530

    Update BinaryToDecimal.java

--- BinaryToDecimal.java
+++ BinaryToDecimal.java
@@ -3,8 +3,15 @@ import java.util.Scanner;
 class BinaryToDecimal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String b = sc.next();
-        int d = Integer.parseInt(b, 2);
+        int b = sc.nextInt();
+        int d = 0;
+        int p = 1;
+        while (b > 0) {
+            int r = b % 10;
+            d = d + r * p;
+            p = p * 2;
+            b = b / 10;
+        }
         System.out.println(d);
     }
 }
