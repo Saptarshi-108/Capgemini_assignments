bc7b04fcea0e4513d0001fdd4d5ff44fddc863ca
Saptarshi Mandal <121552564+Saptarshi-108@users.noreply.github.com>
2026-01-13 00:14:43 +0530

    Add BinaryToDecimal program to convert binary to decimal

---
+++ BinaryToDecimal.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class BinaryToDecimal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String b = sc.next();
+        int d = Integer.parseInt(b, 2);
+        System.out.println(d);
+    }
+}
