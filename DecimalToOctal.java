b3f8786e1a94a4c6658ea3ffe5d4f84659fde337
Saptarshi Mandal <121552564+Saptarshi-108@users.noreply.github.com>
2026-01-13 00:27:09 +0530

    Update DecimalToOctal.java

--- DecimalToOctal.java
+++ DecimalToOctal.java
@@ -4,6 +4,13 @@ class DecimalToOctal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println(Integer.toOctalString(n));
+        int oct = 0;
+        int p = 1;
+        while (n > 0) {
+            oct = oct + (n % 8) * p;
+            p = p * 10;
+            n = n / 8;
+        }
+        System.out.println(oct);
     }
 }
