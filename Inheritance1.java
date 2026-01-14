a86dbc646500e9f7a0050339f98447af77bdeb94
Saptarshi Mandal <121552564+Saptarshi-108@users.noreply.github.com>
2026-01-14 16:21:49 +0530

    Create Inheritance1.java

---
+++ Inheritance1.java
@@ -0,0 +1,32 @@
+package oops;
+
+public class Vehicle{
+	String brand;
+	String color;
+	double price;
+	
+	public void printVehicle() {
+		System.out.println(brand);
+		System.out.println(color);
+		System.out.println(price);
+	}
+	
+	public static void main(String[] args) {
+		Bike b = new Bike("BMW","black",150);
+		b.printVehicle();
+		Vehicle v = new Vehicle();
+		v.brand="honda";
+		v.color="red";
+		v.price=100000;
+		v.printVehicle();
+	}
+}
+
+class Bike extends Vehicle{
+	
+	Bike(String brand, String color, double price){
+		this.brand=brand;
+		this.color=color;
+		this.price=price;
+	}
+}
