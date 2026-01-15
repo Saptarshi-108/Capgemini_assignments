605d264c049a278f11c167eb3319c0bb6dc46d51
Saptarshi Mandal <121552564+Saptarshi-108@users.noreply.github.com>
2026-01-15 09:24:43 +0530

    Update Hierarchical_Inherit.java

--- Hierarchical_Inherit.java
+++ Hierarchical_Inherit.java
@@ -1,27 +1,37 @@
 package oops;
 
 class A {
-    private int x;
-    void setX(int x) { this.x = x; }
-    int getX() { return x; }
+	private int x;
+
+	void setX(int x) {
+		this.x = x;
+	}
+
+	int getX() {
+		return x;
+	}
 }
 
 class B extends A {
-    int sq() { return getX() * getX(); }
+	int sq() {
+		return getX() * getX();
+	}
 }
 
 class C extends A {
-    int cb() { return getX() * getX() * getX(); }
+	int cb() {
+		return getX() * getX() * getX();
+	}
 }
 
 public class Test {
-    public static void main(String[] args) {
-        B b = new B();
-        b.setX(2);
-        System.out.println(b.sq());
+	public static void main(String[] args) {
+		B b = new B();
+		b.setX(2);
+		System.out.println(b.sq());
 
-        C c = new C();
-        c.setX(2);
-        System.out.println(c.cb());
-    }
+		C c = new C();
+		c.setX(2);
+		System.out.println(c.cb());
+	}
 }
