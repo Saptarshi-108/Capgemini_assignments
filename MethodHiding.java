1b6a1da21d6ac1f649368287a1649d47dd1be9e0
Saptarshi Mandal <121552564+Saptarshi-108@users.noreply.github.com>
2026-01-16 11:03:58 +0530

    Create MethodHiding.java

---
+++ MethodHiding.java
@@ -0,0 +1,33 @@
+package oops; 
+
+class Parent{
+	public void home() {
+		System.out.println("blue");
+	}
+	public void home2() {
+		System.out.println("Parent static method");
+	}
+}
+public class Child extends Parent{
+  @Override
+	public void home() {
+		System.out.println("red");
+	}
+	public void home2() {
+		System.out.println("Child static method");
+	}
+	public void bike() {
+		System.out.println("BMW");
+	}
+	
+	public static void main(String args[]) {
+		Parent p = new Parent();
+		p.home();
+		Child c = new Child();
+		c.home();
+		c.bike();
+		Parent p2 = new Child();
+		p.home(); //Child 
+		p.home2(); //Parent
+	}
+}
