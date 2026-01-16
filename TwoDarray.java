2181b76fff20a79450351dce447debf939f87dc1
Saptarshi Mandal <121552564+Saptarshi-108@users.noreply.github.com>
2026-01-16 16:15:22 +0530

    Create TwoDarray.java

---
+++ TwoDarray.java
@@ -0,0 +1,23 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class TwoDarray{
+	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter elements");
+		int[][] a = new int[2][3];
+		
+		for (int i=0;i<a.length;i++) {
+			for (int j=0;j<a[i].length;j++) {
+				a[i][j]=sc.nextInt();
+			}
+		}
+		for (int i=0;i<a.length;i++) {
+			for (int j=0;j<a[i].length;j++) {
+				System.out.println(a[i][j]);
+			}
+		}
+	}
+
+}
