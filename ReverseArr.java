8fce2de87c8d475a394ff0f981a35f4b49051af6
Saptarshi Mandal <121552564+Saptarshi-108@users.noreply.github.com>
2026-01-16 14:32:45 +0530

    Create ReverseArr.java

---
+++ ReverseArr.java
@@ -0,0 +1,26 @@
+package arrays;
+import java.util.Scanner;
+
+public class ReverseArr {
+    public static int[] reverse(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+        return arr;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] array = reverse(arr);
+        for (int i = 0; i < n; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
