22763431c73de71986feee6e1c614ab3880c0efa
Saptarshi Mandal <121552564+Saptarshi-108@users.noreply.github.com>
2026-01-19 09:20:47 +0530

    Create LowerTriangular.java

---
+++ LowerTriangular.java
@@ -0,0 +1,19 @@
+public class LowerTriangular {
+    public static void main(String[] args) {
+        int[][] a = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                if (i >= j)
+                    System.out.print(a[i][j] + " ");
+                else
+                    System.out.print("0 ");
+            }
+            System.out.println();
+        }
+    }
+}
